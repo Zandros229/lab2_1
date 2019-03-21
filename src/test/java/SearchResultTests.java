f49daa00ec69b47243e75e5b38d2250018a3200f
Zandros229 <ernest.figura548@gmail.com>
2019-03-21 17:31:30 +0100

    testy_v1 2.1 write elementInOneElementSequence implementation search method give back wrong key position

--- src/test/java/SearchResultTests.java
+++ src/test/java/SearchResultTests.java
@@ -1,3 +1,5 @@
+import edu.iis.mto.bsearch.BinarySearch;
+import edu.iis.mto.bsearch.SearchResult;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -5,7 +7,12 @@ import org.junit.Test;
 public class SearchResultTests {
     @Test
     public void elementInOneElementSequence(){
+        int[] tabel={6};
+        int key=6;
 
+        SearchResult result= BinarySearch.search(key,tabel);
+        Assert.assertEquals(true ,result.isFound());
+        Assert.assertEquals(0,result.getPosition());
     }
     @Test
     public void elementNotInOneElementSequence(){
