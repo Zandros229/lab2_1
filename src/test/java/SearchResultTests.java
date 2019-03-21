8c5ef2dd1c1ec5964bf2f63519aefb27a035b091
Zandros229 <ernest.figura548@gmail.com>
2019-03-21 17:38:06 +0100

    testy_v1 2.1 add implementation of elementIsFirstElementOfSequence and elementNotInOneElementSequence tests

--- src/test/java/SearchResultTests.java
+++ src/test/java/SearchResultTests.java
@@ -5,22 +5,36 @@ import org.junit.Test;
 
 
 public class SearchResultTests {
+
     @Test
     public void elementInOneElementSequence(){
         int[] tabel={6};
-        int key=6;
+        final int KEY=6;
+        final int EXPECTED=-1;
 
-        SearchResult result= BinarySearch.search(key,tabel);
+        SearchResult result= BinarySearch.search(KEY,tabel);
         Assert.assertEquals(true ,result.isFound());
-        Assert.assertEquals(0,result.getPosition());
+        Assert.assertEquals(EXPECTED,result.getPosition());
     }
     @Test
     public void elementNotInOneElementSequence(){
+        int[] tabel={4};
+        final int KEY=6;
+        final int EXPECTED=-1;
 
+        SearchResult result= BinarySearch.search(KEY,tabel);
+        Assert.assertEquals(false ,result.isFound());
+        Assert.assertEquals(EXPECTED,result.getPosition());
     }
     @Test
     public void elementIsFirstElementOfSequence(){
+        int[] tabel={4,5,6,7};
+        final int KEY=4;
+        final int EXPECTED=0;
 
+        SearchResult result= BinarySearch.search(KEY,tabel);
+        Assert.assertEquals(true ,result.isFound());
+        Assert.assertEquals(EXPECTED,result.getPosition());
     }
     @Test
     public void elementIsLastElementOfSequence(){
