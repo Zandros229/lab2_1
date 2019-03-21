d7dce8510a8a2789c432e6d6938e9cc03992bcf2
Zandros229 <ernest.figura548@gmail.com>
2019-03-21 17:19:50 +0100

    2.1 Write Method names without Implementation

---
+++ src/test/java/SearchResultTests.java
@@ -0,0 +1,32 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+
+public class SearchResultTests {
+    @Test
+    public void elementInOneElementSequence(){
+
+    }
+    @Test
+    public void elementNotInOneElementSequence(){
+
+    }
+    @Test
+    public void elementIsFirstElementOfSequence(){
+
+    }
+    @Test
+    public void elementIsLastElementOfSequence(){
+
+    }
+    @Test
+    public void elementIsMiddleElementOfSequence(){
+
+    }
+    @Test
+    public void elementNotInSequence(){
+
+    }
+
+
+}
