37a302f81c9c478acddf901667ac1e1bd6afed41
Zandros229 <ernest.figura548@gmail.com>
2019-03-21 17:48:16 +0100

    testy_v1 2.1 fix code format

--- src/test/java/SearchResultTests.java
+++ src/test/java/SearchResultTests.java
@@ -7,64 +7,69 @@ import org.junit.Test;
 public class SearchResultTests {
 
     @Test
-    public void elementInOneElementSequence(){
-        int[] tabel={6};
-        final int KEY=6;
-        final int EXPECTED=0;
+    public void elementInOneElementSequence() {
+        int[] tabel = {6};
+        final int KEY = 6;
+        final int EXPECTED = 0;
 
-        SearchResult result= BinarySearch.search(KEY,tabel);
-        Assert.assertEquals(true ,result.isFound());
-        Assert.assertEquals(EXPECTED,result.getPosition());
+        SearchResult result = BinarySearch.search(KEY, tabel);
+        Assert.assertEquals(true, result.isFound());
+        Assert.assertEquals(EXPECTED, result.getPosition());
     }
-    @Test
-    public void elementNotInOneElementSequence(){
-        int[] tabel={4};
-        final int KEY=6;
-        final int EXPECTED=-1;
 
-        SearchResult result= BinarySearch.search(KEY,tabel);
-        Assert.assertEquals(false ,result.isFound());
-        Assert.assertEquals(EXPECTED,result.getPosition());
+    @Test
+    public void elementNotInOneElementSequence() {
+        int[] tabel = {4};
+        final int KEY = 6;
+        final int EXPECTED = -1;
+
+        SearchResult result = BinarySearch.search(KEY, tabel);
+        Assert.assertEquals(false, result.isFound());
+        Assert.assertEquals(EXPECTED, result.getPosition());
     }
-    @Test
-    public void elementIsFirstElementOfSequence(){
-        int[] tabel={4,5,6,7};
-        final int KEY=4;
-        final int EXPECTED=0;
 
-        SearchResult result= BinarySearch.search(KEY,tabel);
-        Assert.assertEquals(true ,result.isFound());
-        Assert.assertEquals(EXPECTED,result.getPosition());
+    @Test
+    public void elementIsFirstElementOfSequence() {
+        int[] tabel = {4, 5, 6, 7};
+        final int KEY = 4;
+        final int EXPECTED = 0;
+
+        SearchResult result = BinarySearch.search(KEY, tabel);
+        Assert.assertEquals(true, result.isFound());
+        Assert.assertEquals(EXPECTED, result.getPosition());
     }
-    @Test
-    public void elementIsLastElementOfSequence(){
-        int[] tabel={4,5,6,7};
-        final int KEY=7;
-        final int EXPECTED=tabel.length-1;
 
-        SearchResult result= BinarySearch.search(KEY,tabel);
-        Assert.assertEquals(true ,result.isFound());
-        Assert.assertEquals(EXPECTED,result.getPosition());
+    @Test
+    public void elementIsLastElementOfSequence() {
+        int[] tabel = {4, 5, 6, 7};
+        final int KEY = 7;
+        final int EXPECTED = tabel.length - 1;
+
+        SearchResult result = BinarySearch.search(KEY, tabel);
+        Assert.assertEquals(true, result.isFound());
+        Assert.assertEquals(EXPECTED, result.getPosition());
     }
-    @Test
-    public void elementIsMiddleElementOfSequence(){
-        int[] tabel={3,4,5,6,7};
-        final int KEY=5;
-        final int EXPECTED=2;
 
-        SearchResult result= BinarySearch.search(KEY,tabel);
-        Assert.assertEquals(true ,result.isFound());
-        Assert.assertEquals(EXPECTED,result.getPosition());
+    @Test
+    public void elementIsMiddleElementOfSequence() {
+        int[] tabel = {3, 4, 5, 6, 7};
+        final int KEY = 5;
+        final int EXPECTED = 2;
+
+        SearchResult result = BinarySearch.search(KEY, tabel);
+        Assert.assertEquals(true, result.isFound());
+        Assert.assertEquals(EXPECTED, result.getPosition());
     }
-    @Test
-    public void elementNotInSequence(){
-        int[] tabel={3,4,5,6,7};
-        final int KEY=8;
-        final int EXPECTED=-1;
 
-        SearchResult result= BinarySearch.search(KEY,tabel);
-        Assert.assertEquals(false ,result.isFound());
-        Assert.assertEquals(EXPECTED,result.getPosition());
+    @Test
+    public void elementNotInSequence() {
+        int[] tabel = {3, 4, 5, 6, 7};
+        final int KEY = 8;
+        final int EXPECTED = -1;
+
+        SearchResult result = BinarySearch.search(KEY, tabel);
+        Assert.assertEquals(false, result.isFound());
+        Assert.assertEquals(EXPECTED, result.getPosition());
     }
 
 
